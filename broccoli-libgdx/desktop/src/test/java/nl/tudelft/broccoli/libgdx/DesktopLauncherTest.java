7a08634eaa064e8b4de2e6e07e284aac0dd5cf75
Fabian Mastenbroek <mail.fabianm@gmail.com>
2017-10-25 12:49:26 +0200

    chore: Add headless testing for libgdx:desktop module
    This change updates the Travis CI configuration, so that we can test the
    libgdx:desktop module via an X virtual framebuffer.

--- broccoli-libgdx/desktop/src/test/java/nl/tudelft/broccoli/libgdx/DesktopLauncherTest.java
+++ broccoli-libgdx/desktop/src/test/java/nl/tudelft/broccoli/libgdx/DesktopLauncherTest.java
@@ -2,11 +2,6 @@ package nl.tudelft.broccoli.libgdx;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import nl.tudelft.broccoli.core.config.Configuration;
-import nl.tudelft.broccoli.core.config.Property;
 import org.junit.Test;
 
 /**
@@ -19,31 +14,13 @@ public class DesktopLauncherTest {
      */
     @Test
     public void smokeTest() {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Broccoli";
-        config.width = 10;
-        config.height = 10;
-        config.resizable = false;
-
-        Configuration gameConfig = new Configuration() {
-            @Override
-            public <T> T get(Property<T> property, T defaultValue) {
-                return defaultValue;
-            }
-
-            @Override
-            public boolean exists(Property<?> property) {
-                return false;
-            }
-        };
-
         // This smoke test is so disgusting, you would not even consider giving
         // it to your mother-in-law.
         // But it shows that the game runs and exits without crashing.
         assertThatCode(() -> {
-            Application app = new LwjglApplication(new Broccoli(gameConfig), config);
-            Thread.sleep(800);
-            app.exit();
+            DesktopLauncher.main(new String[] {});
+            // Wait two minutes into the game
+            Thread.sleep(10000);
         }).doesNotThrowAnyException();
     }
 }
